efdac34e46aa5679cc2c66e5b316f72fd246da68
Adam <adamgensh@gmail.com>
2024-04-04 00:29:56 +0300

    Add polling to sync players

--- static/index.js
+++ static/index.js
@@ -1,24 +1,13 @@
 import { player } from "./mp4.js";
 import { animItem } from "./animation.js";
 
-// add when debugging animItem.goToAndStop(6 * 1000);
-player.on("timeupdate", (e) => {
-  if (e.manuallyTriggered) {
-    if (player.paused()) {
-      animItem.goToAndStop(player.currentTime() * 1000);
-    } else {
-      animItem.goToAndPlay(player.currentTime() * 1000);
-    }
-  } else if (player.scrubbing()) {
-    player.scrubbing();
+const polling = () => {
+  if (player.paused()) {
     animItem.goToAndStop(player.currentTime() * 1000);
+  } else {
+    animItem.goToAndPlay(player.currentTime() * 1000);
   }
-});
-player.on("play", () => {
-  animItem.goToAndPlay(player.currentTime() * 1000);
-  // $("#container").get(0).requestFullscreen();
-});
-player.on("pause", () => {
-  animItem.goToAndStop(player.currentTime() * 1000);
-  // document.exitFullscreen();
-});
+  setTimeout(polling);
+};
+
+polling();

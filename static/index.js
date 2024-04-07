12e9de7e54aab600b90a1ee7909c4d872122df00
Adam <adamgensh@gmail.com>
2024-04-07 20:23:47 +0300

    Improve polling and simplify it's stack trace

--- static/index.js
+++ static/index.js
@@ -1,13 +1,19 @@
 import { player } from "./mp4.js";
 import { animItem } from "./animation.js";
 
-const polling = () => {
+const poll = () => {
   if (player.paused()) {
     animItem.goToAndStop(player.currentTime() * 1000);
   } else {
     animItem.goToAndPlay(player.currentTime() * 1000);
   }
-  setTimeout(polling);
 };
 
-polling();
+const initPolling = async () => {
+  while (true) {
+    poll();
+    await new Promise((resolve) => setTimeout(resolve));
+  }
+};
+
+initPolling();

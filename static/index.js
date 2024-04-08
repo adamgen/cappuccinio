cf711f0b268f25a6311d4b6194a7e28cf6da31d6
Adam <adamgensh@gmail.com>
2024-04-08 08:05:36 +0300

    Add debug box

--- static/index.js
+++ static/index.js
@@ -17,3 +17,31 @@ const initPolling = async () => {
 };
 
 initPolling();
+
+let isDebugMode = window.localStorage.getItem("is_debug_mode") === "true";
+
+let bounce = false;
+document.addEventListener("touchstart", function (e) {
+  if (e.touches.length <= 1) {
+    return;
+  }
+  if (!bounce) {
+    bounce = true;
+    setTimeout(() => {
+      bounce = false;
+    }, 300);
+    return;
+  }
+  isDebugMode = !isDebugMode;
+  setDebugModeUI();
+  window.localStorage.setItem("is_debug_mode", isDebugMode + "");
+});
+
+const setDebugModeUI = () => {
+  if (isDebugMode) {
+    document.getElementById("debug-box").classList.remove("hidden");
+  } else {
+    document.getElementById("debug-box").classList.add("hidden");
+  }
+};
+setDebugModeUI();

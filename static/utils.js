0762f6bbc76547c6cee577a0c6ce21d0088c27a0
Adam <adamgensh@gmail.com>
2024-11-29 17:29:53 +0200

    Upgrade wrangler version

--- static/utils.js
+++ static/utils.js
@@ -1,7 +1,7 @@
 export const isDev = window.location.host.match(/0.0.0.0(:\d{4})/) !== null;
 
 export const client = isDev
-  ? "tlv-bday"
+  ? "xtra"
   : window.location.host.replace(".cappuccin.io", "");
 
 export const getClientRun = () => import(`./clients/${client}/run.js`);
